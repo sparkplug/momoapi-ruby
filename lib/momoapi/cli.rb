236646e7bffe5405f9c5bb8244f7103f8127e923
NLSanyu <lydiansanyu@gmail.com>
2020-02-05 17:09:08 +0300

    Add CLI executable file

---
+++ lib/momoapi/cli.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'momoapi'
+
+module Momoapi
+  # class that holds CLI commands
+  class CLI
+    def self.create_sandbox_user
+      puts 'Creating MoMo user'
+    end
+  end
+end
