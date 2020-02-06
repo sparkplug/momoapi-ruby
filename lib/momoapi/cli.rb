e3c766db4dc19cf58dac4d51cb2fb23c9e2ab769
NLSanyu <lydiansanyu@gmail.com>
2020-02-06 18:12:00 +0300

    Make API request to create user

--- lib/momoapi/cli.rb
+++ lib/momoapi/cli.rb
@@ -1,12 +1,28 @@
 # frozen_string_literal: true
 
 require 'momoapi'
+require 'httparty'
 
 module Momoapi
   # class that holds CLI commands
   class CLI
-    def self.create_sandbox_user
+    def self.create_sandbox_user(host, key)
       puts 'Creating MoMo user'
+
+      uuid = UUID.new
+      id = uuid.generate
+      
+      body = { "providerCallbackHost": host }
+      headers = {
+        "X-Reference-Id" => id,
+        "Ocp-Apim-Subscription-Key" => key,
+        "Content-Type" => "application/json"
+        }
+
+        HTTParty.post("https://sandbox.momodeveloper.mtn.com/v1_0/apiuser", 
+          headers: headers,
+          body: body
+        )
     end
   end
 end
--- momoapi-ruby.gemspec
+++ momoapi-ruby.gemspec
@@ -36,4 +36,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'pry', '~> 0.12'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'httparty', '~> 0.17'
+  spec.add_development_dependency 'uuid', '~> 2.3'
 end
