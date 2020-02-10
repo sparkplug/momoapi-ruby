0adcecc5bd4e5244639172a9218f0fcdeea75295
NLSanyu <lydiansanyu@gmail.com>
2020-02-10 16:13:42 +0300

    Add option parsing

--- lib/momoapi/cli.rb
+++ lib/momoapi/cli.rb
@@ -5,14 +5,14 @@ require 'httparty'
 require 'uuid'
 
 module Momoapi
-  # class that holds CLI commands
   class CLI
-    def create_sandbox_user(host, key)
+    def create_sandbox_user(option)
       puts 'Creating MoMo Sandbox user'
       uuid = UUID.new.generate
-      response = make_api_request(host, key, uuid)
-
-      puts response
+      host = option[:host]
+      key = option[:key]
+      make_api_request(host, key, uuid)
+      response = generate_api_key(uuid, key)
     end
 
     def make_api_request(host, key, id)
@@ -22,9 +22,14 @@ module Momoapi
         'Ocp-Apim-Subscription-Key' => key,
         'Content-Type' => 'application/json'
       }
-      HTTParty.post('https://sandbox.momodeveloper.mtn.com/v1_0/apiuser',
+      response = HTTParty.post('https://sandbox.momodeveloper.mtn.com/v1_0/apiuser',
                     body: body,
                     headers: headers)
+      if response.code == 201
+        puts "Sandbox user created"
+      else 
+        puts "Error creating user"
+      end
     end
 
     def generate_api_key(uuid, key)
@@ -32,7 +37,12 @@ module Momoapi
         'Ocp-Apim-Subscription-Key' => key
       }
       url = "https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/#{uuid}/apikey"
-      HTTParty.post(url, headers: headers)
+      response = HTTParty.post(url, headers: headers)
+      if response.code == 201
+        puts response.parsed_response
+      else 
+        puts "Error creating API key"
+      end
     end
   end
 end
