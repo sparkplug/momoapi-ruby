c773f442ca1a906983cd2a1b138cf3aa811e68bc
NLSanyu <lydiansanyu@gmail.com>
2020-02-18 17:10:20 +0300

    Add remittance and disbursement classes

---
+++ lib/momoapi-ruby/disbursement.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'momoapi-ruby/config'
+require 'momoapi-ruby/client'
+
+module Momoapi
+  class Disbursement < Client
+  end
+end
---
+++ lib/momoapi-ruby/remittance.rb
@@ -0,0 +1,7 @@
+require 'momoapi-ruby/config'
+require 'momoapi-ruby/client'
+
+module Momoapi
+  class Remittance < Client
+  end
+end
