432a5bd5c78792bb4a7193c90ba451265c995c27
NLSanyu <lydiansanyu@gmail.com>
2020-02-21 11:16:35 +0300

    Add common functionality to all products

--- lib/momoapi-ruby/disbursement.rb
+++ lib/momoapi-ruby/disbursement.rb
@@ -5,5 +5,21 @@ require 'momoapi-ruby/client'
 
 module Momoapi
   class Disbursement < Client
+    def get_auth_token
+      path = 'disbursement/token'
+      super(path, Momoapi.config.disbursement_primary_key)
+    end
+
+    def get_balance
+      path = '/disbursement/v1_0/account/balance'
+      super(path, Momoapi.config.disbursement_primary_key)
+    end
+
+    def get_transaction_status(transaction_id)
+      path = "/disbursement/v1_0/transfer/#{transaction_id}"
+      super(path, Momoapi.config.disbursement_primary_key)
+    end
+
+    def transfer; end
   end
 end
--- lib/momoapi-ruby/remittance.rb
+++ lib/momoapi-ruby/remittance.rb
@@ -5,5 +5,21 @@ require 'momoapi-ruby/client'
 
 module Momoapi
   class Remittance < Client
+    def get_auth_token
+      path = 'remittance/token'
+      super(path, Momoapi.config.remittance_primary_key)
+    end
+
+    def get_balance
+      path = '/remittance/v1_0/account/balance'
+      super(path, Momoapi.config.remittance_primary_key)
+    end
+
+    def get_transaction_status(transaction_id)
+      path = "/remittance/v1_0/transfer/#{transaction_id}"
+      super(path, Momoapi.config.remittance_primary_key)
+    end
+
+    def transfer; end
   end
 end
